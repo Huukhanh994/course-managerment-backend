85ebecd882eecc112a4e4f8fc8b70b14e2af8a06
HO HUU KHANH <46079705+Huukhanh994@users.noreply.github.com>
2021-05-08 19:02:13 +0700

    Fix HasFactory has not found

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -9,6 +9,7 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * Class Question
